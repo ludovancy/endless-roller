9245b934000bdb2dc0d37a6aadd4426847732663
ludovancy <191650357+ludovancy@users.noreply.github.com>
2024-12-23 00:27:13 -0400

    Well, something is happenings

--- src/player.gd
+++ src/player.gd
@@ -1,19 +1,8 @@
 extends Node3D
 
-signal request_new_path(paths_remaining)
-
 @onready var character_body_3d: CharacterBody3D = $CharacterBody3D
 @export var road_width : float = 9.0
-@export var init_speed : float = 0.2
-var speed = init_speed
 var dragging: bool = false
-
-var t = 0
-var curve : Curve3D = null
-var next_curves : Array = []
-
-func _ready() -> void:
-	get_parent().connect("new_curve", set_new_curve)
 
 func _input(event: InputEvent) -> void:
 	var screen_x : float = get_viewport().get_visible_rect().size.x
@@ -32,19 +21,10 @@ func _input(event: InputEvent) -> void:
 		new_position += road_width / 2
 		character_body_3d.position.x = new_position
 
-func _process(delta: float) -> void:
-	if not curve:
-		print("No curve")
-		return
-	t += delta * speed
-	transform = curve.sample_baked_with_rotation(t * curve.get_baked_length(), true)
-	if t > 1:
-		t -= 1
-		curve = next_curves.pop_front()
-		request_new_path.emit(len(next_curves))
-
-func set_new_curve(new_curve : Curve3D):
-	if curve == null:
-		curve = new_curve
-		return
-	next_curves.append(new_curve)
+	if event is InputEventKey and event.pressed:
+		if event.is_action_pressed("ui_left"):
+			$Camera3D.rotation.y = 90
+		if event.is_action_pressed("ui_right"):
+			$Camera3D.rotation.y = -90
+		if event.is_action_pressed("ui_up"):
+			$Camera3D.rotation.y = 0
--- src/world.gd
+++ src/world.gd
@@ -1,11 +1,15 @@
 extends Node3D
 
-signal new_curve
-
-@export var max_paths : int = 2
 @export var points_in_curve : int = 10
+@export var init_speed : float = 0.1
+var speed = init_speed
+var t : float = 0
+
 
 @onready var player: Node3D = $Player
+@onready var path_root: Node3D = $PathRoot
+
+@export var max_paths : int = 3
 @onready var path_3d: Path3D = $PathRoot/Path3D
 @onready var path_3d_2: Path3D = $PathRoot/Path3D2
 @onready var path_3d_3: Path3D = $PathRoot/Path3D3
@@ -15,29 +19,44 @@ var path_index := 0
 
 func _ready():
 	# generate max paths
-	handle_new_path(0)
-	player.connect("request_new_path", handle_new_path)
+	for i in range(max_paths):
+		generate_path(i)
 
-func handle_new_path(paths_remaining):
-	for i in range(max_paths - paths_remaining):
-		generate_path()
-
-func generate_path():
-	# get starting position of new path
-	var starting_position : Vector3 = paths[path_index].position
-	starting_position += paths[path_index].curve.get_point_position(
-				paths[path_index].curve.point_count - 1
-			)
-	# start generating the new path 
-	path_index = (path_index + 1) % len(paths)
-	paths[path_index].position = starting_position
-	paths[path_index].curve.clear_points()
+func generate_path(index:int):
+	paths[index].curve.clear_points()
 	# procedural generation
 	for i in range(points_in_curve):
 		if i == 0:
-			paths[path_index].curve.add_point(Vector3.ZERO)
+			paths[index].curve.add_point(Vector3.ZERO)
 			continue
-		paths[path_index].curve.add_point(
+		paths[index].curve.add_point(
 		Vector3(i*10, randf_range(0,2), randf_range(-3, 3)))
-	# send the curve to the player
-	new_curve.emit(paths[path_index].curve)
+	# place path
+	paths[index].position = paths[(index - 1) % max_paths].position
+	var last_point : Vector3 = Vector3.ZERO
+	if paths[(index - 1) % max_paths].curve.point_count > 0:
+		last_point = paths[(index - 1) % max_paths].curve.get_point_position(
+			paths[(index - 1) % max_paths].curve.point_count - 1
+		)
+	paths[index].position += last_point
+
+func cycle_path():
+	# get starting position of new path
+	var starting_position : = path_root.position
+	# move everything back
+	path_root.position = Vector3.ZERO
+	for i in range(max_paths):
+		paths[i].position = Vector3.ZERO + starting_position
+	# advance
+	path_index = (path_index + 1) % max_paths
+	generate_path((path_index - 2) % max_paths)
+
+func _process(delta: float) -> void:
+	t += delta * speed
+	var move := paths[path_index].curve.sample_baked_with_rotation(
+		t * paths[path_index].curve.get_baked_length(), true)
+	path_root.transform = move.inverse()
+	player.transform.basis = move.basis
+	if t >= 1.0:
+		t -= 1.0
+		cycle_path()
