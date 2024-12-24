8e18796978e59446d5369e1f5863f6be1a26ce97
ludovancy <191650357+ludovancy@users.noreply.github.com>
2024-12-24 15:47:09 -0400

    Create path scene

--- src/world.gd
+++ src/world.gd
@@ -1,7 +1,7 @@
 extends Node3D
 
 @export var points_in_curve : int = 10
-@export var init_speed : float = 0.15
+@export var init_speed : float = 0.1
 var speed = init_speed
 var t : float = 0
 
@@ -9,12 +9,20 @@ var t : float = 0
 @onready var player: Node3D = $Player
 @onready var path_root: Node3D = $PathRoot
 
-@export var max_paths : int = 3
-@onready var path_3d: Path3D = $PathRoot/Path3D
-@onready var path_3d_2: Path3D = $PathRoot/Path3D2
-@onready var path_3d_3: Path3D = $PathRoot/Path3D3
+@export var max_paths : int = 4
 
-@onready var paths : Array[Path3D] = [path_3d, path_3d_2, path_3d_3]
+@onready var node_3d: Node3D = $PathRoot/Node3D
+@onready var node_3d_2: Node3D = $PathRoot/Node3D2
+@onready var node_3d_3: Node3D = $PathRoot/Node3D3
+@onready var node_3d_4: Node3D = $PathRoot/Node3D4
+
+@onready var path_3d: Path3D = $PathRoot/Node3D/Path3D
+@onready var path_3d_2: Path3D = $PathRoot/Node3D2/Path3D2
+@onready var path_3d_3: Path3D = $PathRoot/Node3D4/Path3D3
+@onready var path_3d_4: Path3D = $PathRoot/Node3D3/Path3D4
+
+@onready var paths : Array[Path3D] = [path_3d, path_3d_2, path_3d_3, path_3d_4]
+@onready var nodes : Array[Node3D] = [node_3d, node_3d_2, node_3d_3, node_3d_4]
 var path_index := 0
 
 func _ready():
@@ -34,22 +42,29 @@ func generate_path(index:int):
 	update_control_points(0.7, paths[index].curve)
 	# place path
 	var prev_path_index = posmod((index - 1), max_paths)
-	var prev_path_pos := paths[prev_path_index].position
+	var prev_path_pos := nodes[prev_path_index].position
 	var last_point : Vector3 = Vector3.ZERO
 	if paths[prev_path_index].curve.point_count > 0:
 		last_point = paths[prev_path_index].curve.get_point_position(
 			paths[prev_path_index].curve.point_count - 1
 		)
-	paths[index].position = prev_path_pos + last_point
+	last_point = Vector3(0,0,(points_in_curve - 1) * 10)
+	if (paths[prev_path_index].curve.point_count == 0):
+		last_point = Vector3.ZERO
+	nodes[index].position = prev_path_pos + last_point
+	#paths[index].position = Vector3.ZERO
+	print(paths[index].position)
 
 func cycle_path():
 	var initial_location : Vector3 = \
 	paths[path_index].curve.get_point_position(
 		paths[path_index].curve.point_count - 1
 	)
+	print(initial_location)
+	initial_location = Vector3(0,0,(points_in_curve - 1) * 10)
 	# move everything back
 	for i in range(max_paths):
-		paths[i].translate(-1 * initial_location)
+		nodes[i].translate(-1 * initial_location)
 	# advance
 	path_index = (path_index + 1) % max_paths
 	generate_path(posmod((path_index - 2), max_paths)) # no negative numbers
@@ -62,8 +77,9 @@ func _process(delta: float) -> void:
 	var move := paths[path_index].curve.sample_baked_with_rotation(
 		t * paths[path_index].curve.get_baked_length(), true)
 	print(move)
-	path_root.transform = move.inverse()
-	#path_root.transform = Transform3D(Basis(), Vector3(0, 0, -t * 90))
+	#path_root.transform = move.inverse()
+	path_root.transform = Transform3D(Basis(), 
+		Vector3(0, 0, -t * (points_in_curve - 1) * 10))
 
 #Convert Catmull-Rom to Bezier (Curve3D)
 #For more information check out this paper:
